41f9837818ced4baa40330cd7744ca2ea97ac7ae
Caleb Morris <caleb.morris.g@gmail.com>
2015-06-26 22:54:51 -0700

    Cleaned up to simple example

--- app/routes/Application.jsx
+++ app/routes/Application.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { RouteHandler } from 'react-router';
-import MainMenu from 'components/MainMenu';
 
 const Application = React.createClass({
 
   render() {
     return (
       <div>
-        <h1>{'react-starter'}</h1>
-        <MainMenu />
         <RouteHandler />
       </div>
     );
