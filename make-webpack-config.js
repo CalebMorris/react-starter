e322c09e5264ac6933c84402a5374b8f32f6c8f9
Caleb Morris <caleb.morris.g@gmail.com>
2015-07-11 23:36:12 -0700

    Use strong key for attribute when possible [LINT]

--- make-webpack-config.js
+++ make-webpack-config.js
@@ -10,31 +10,31 @@ module.exports = function(options) {
     // second : options.prerender ? './config/secondPrerenderer' : './config/secondApp'
   };
   var loaders = {
-    'jsx' : options.hotComponents ?
+    jsx : options.hotComponents ?
       ['react-hot-loader', 'babel-loader?stage=0'] :
       'babel-loader?stage=0',
-    'js' : {
+    js : {
       loader : 'babel-loader?stage=0',
       include : path.join(__dirname, 'app'),
     },
-    'json' : 'json-loader',
-    'coffee' : 'coffee-redux-loader',
-    'json5' : 'json5-loader',
-    'txt' : 'raw-loader',
+    json : 'json-loader',
+    coffee : 'coffee-redux-loader',
+    json5 : 'json5-loader',
+    txt : 'raw-loader',
     'png|jpg|jpeg|gif|svg' : 'url-loader?limit=10000',
     'woff|woff2' : 'url-loader?limit=100000',
     'ttf|eot' : 'file-loader',
     'wav|mp3' : 'file-loader',
-    'html' : 'html-loader',
+    html : 'html-loader',
     'md|markdown' : ['html-loader', 'markdown-loader'],
   };
   var cssLoader = options.minimize ?
     'css-loader?module' :
     'css-loader?module&localIdentName=[path][name]---[local]---[hash:base64:5]';
   var stylesheetLoaders = {
-    'css' : cssLoader,
-    'less' : [cssLoader, 'less-loader'],
-    'styl' : [cssLoader, 'stylus-loader'],
+    css : cssLoader,
+    less : [cssLoader, 'less-loader'],
+    styl : [cssLoader, 'stylus-loader'],
     'scss|sass' : [cssLoader, 'sass-loader'],
   };
   var additionalLoaders = [
